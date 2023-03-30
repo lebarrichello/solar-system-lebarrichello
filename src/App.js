b67ca6da077bee7d7407476eefb6d422762e5a4a
lebarrichello <leticia.barrichello@gmail.com>
2023-03-30 15:27:02 -0300

    requisito 5

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
 import Title from './components/Title';
+import PlanetCard from './components/PlanetCard';
 
 class App extends React.Component {
   render() {
@@ -10,6 +11,7 @@ class App extends React.Component {
         <Header />
         <SolarSystem />
         <Title />
+        <PlanetCard />
       </>
     );
   }
---
+++ src/components/PlanetCard.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class PlanetCard extends React.Component {
+  render() {
+    const { planetName, planetImage } = this.props;
+    return (
+      <>
+        <div data-testid="planet-card" />
+        <p data-testid="planet-name">{planetName}</p>
+        <img
+          src={ planetImage }
+          alt={ `Planeta ${planetName}` }
+        />
+
+      </>
+    );
+  }
+}
+
+PlanetCard.propTypes = {
+  planetName: PropTypes.string.isRequired,
+  planetImage: PropTypes.string.isRequired,
+};
+export default PlanetCard;
