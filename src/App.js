5f0c3e2ffecdf687b896d3488553ffd80d4d461f
lebarrichello <leticia.barrichello@gmail.com>
2023-03-30 14:53:28 -0300

    requisito  2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
   render() {
     return (
-      <Header />
-
+      <>
+        <Header />
+        <SolarSystem />
+      </>
     );
   }
 }
-
 export default App;
