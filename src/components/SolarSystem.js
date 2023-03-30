b9b3bd3d4433a42fc226b86a486377875f7e33b4
lebarrichello <leticia.barrichello@gmail.com>
2023-03-30 15:10:00 -0300

    requisito 4

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Title from './Title';
 
 class SolarSystem extends React.Component {
   render() {
     return (
-      <div data-testid="solar-system" />
+      <>
+        <div data-testid="solar-system" />
+        <Title headline="Planetas" />
+
+      </>
+
     );
   }
 }
