7d65d3d8ea9f8cb110371e63874f556c5875fa3e
lebarrichello <leticia.barrichello@gmail.com>
2023-03-30 21:17:41 -0300

    estilizando css header

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
 import Missions from './components/Missions';
+import './components/component.css';
 
 class App extends React.Component {
   render() {
